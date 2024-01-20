50d19932d990e4d93386da980e67edc1de70fc84
dcjh <140426836+dcjh@users.noreply.github.com>
2024-01-21 01:32:53 +0800

    Initial Commit

---
+++ BlitzBlitz/BlitzBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlitzBlitzApp.swift
+//  BlitzBlitz
+//
+//  Created by Darrick Chew on 21/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BlitzBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
