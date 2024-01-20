906a0bde99f4a8e7752728f8bfb5443d392d216d
dcjh <140426836+dcjh@users.noreply.github.com>
2024-01-21 01:42:12 +0800

    d

---
+++ BlitzBlitz/StudyTest/App/StudyTestApp.swift
@@ -0,0 +1,33 @@
+//
+//  StudyTestApp.swift
+//  StudyTest
+//
+//  Created by Owen Nigel on 17/1/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+
+@main
+struct StudyTestApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @StateObject var viewModel = AuthViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            CurrentView()
+                .environmentObject(viewModel)
+        }
+    }
+}
